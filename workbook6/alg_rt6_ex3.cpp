4dc1096ad63045a27789d44fad7756f6e2101e7d
yes? <you@example.com>
2025-11-28 12:10:53 +0300

    update workbook6

---
+++ workbook6/alg_rt6_ex3.cpp
@@ -0,0 +1,44 @@
+/*
+Задача «сумма двух». Вернуть индексы двух чисел в неотсортирован¬ном массиве, 
+которые в сумме дают заданное значение. Решить задачу на основе хеш-таблицы. 
+Например, для массива [-1, 2, 3, 4, 7] 
+и заданного значения 5 выводятся индексы 1 и 2 (поскольку 2+3=5).
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+using namespace std;
+
+vector<int> formHashTable(unordered_map<int, int>& hashTable, vector<int>& inputNumbers, int target) {
+    int sub;
+    for (auto i = 0; i < inputNumbers.size(); i++) {
+        if (inputNumbers[i] <= target) {
+            sub = target - inputNumbers[i];
+            if (hashTable.count(sub) > 0) {
+                return {hashTable[sub], i};
+            }
+            hashTable[inputNumbers[i]] = i;
+        }
+
+    }
+    return {-1, -1};
+}
+ 
+
+int main() {
+    vector<int> inputNumbers, answer;
+    int len, target;
+    cin >> len >> target;
+    for (int i = 0; i < len; i++) {
+        int num;
+        cin >> num;
+        inputNumbers.push_back(num);
+    }
+    unordered_map<int, int> hashTable;
+    for (auto el : formHashTable(hashTable, inputNumbers, target)) cout << el << " ";
+    
+    return 0;
+}
