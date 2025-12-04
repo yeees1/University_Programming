5187ff4452f93124902f24cab83d8fa8c339f749
yes? <you@example.com>
2025-12-04 16:50:18 +0300

    update workbook6

---
+++ workbook6/alg_rt6_ex6.cpp
@@ -0,0 +1,35 @@
+/*
+	Напишите с использованием хеш-таблицы функцию, которая возвращает первый неповторяющийся
+     символ в строке. Например, в строке "minimum" есть два неповторяющихся символа - "n" и "u ", поэтому 
+     ваша функция должна возвратить "n", так как он встречается первым.
+     Временная сложность вашей функции должна быть равна O(N).
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+string formHashTable(string inputString) {
+    unordered_map<char, int> hashTable;
+    string answer = "повторяющихся символов нет";
+    for (auto element : inputString) hashTable[element]++;
+    for (auto element : inputString) {
+        if (hashTable[element] == 1) {
+            answer = element;
+            return answer;
+        }
+    }
+    return answer;
+}
+
+	
+
+int main() {
+	string inputString;
+    cout << "введите строку ";
+	cin >> inputString;
+    cout << formHashTable(inputString) << endl;
+    return 0;
+}
