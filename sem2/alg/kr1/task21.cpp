feb414a08b70a35108f96142cc75561b9913e8e6
yes? <you@example.com>
2026-02-28 13:15:35 +0300

    AlgSem2Kr1Wb1.2T1

--- sem2/alg/kr1/task21.cpp
+++ sem2/alg/kr1/task21.cpp
@@ -1,7 +1,6 @@
-//СКОРЕЕ ВСЕГО НЕПРАВИЛЬНО
-
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -10,67 +9,92 @@ struct Node {
     Node* next;
 };
 
+Node* createNode(int value) {
+    Node* node = new Node;
+    node->data = value;
+    node->next = nullptr;
+    return node;
+}
 
-string checkList(Node* head, Node* nodes,  int n) {
-    Node* current = head;
-    for (int i = 0; i < n-1; i++) {
-        if (current == nullptr) return "early null";
-        if (current->next == nullptr){
-            if (i + 1 == n) return "ok";
-            return "earrly null";
-        }
-        int tempIdx = (current->next) - head;
-        if (tempIdx > i + 1) return "proskok";
-        if (tempIdx <= i) return "pereskok";
-        current = current->next;
-
+Node* constructList(vector<int> &values) {
+    Node* head = createNode(values[0]);
+    Node* temp = createNode(values[1]);
+    head->next = temp;
+    for (int i = 2; i < values.size(); i++){
+        Node* tempNode = createNode(values[i]);
+        temp->next = tempNode;
+        temp = tempNode;
     }
-    return "ok";
+    return head;
+}
 
+bool hasCycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+bool isCountList(Node* head, int n) {
+    int counter = 0;
+    Node* current = head;
+    while (current != nullptr) {
+        counter++;
+        current = current->next;
+    }
+    return counter < n;
+}
+
+string checkList(Node* head, int n) {
+    Node* current = head;
+    bool flag1, flag2;
+    flag1 = hasCycle(current);
+    if (flag1) return "prereskok";
+    flag2 = isCountList(current, n);
+    if (flag2) return "proskok";
+    return "ok";
+}
+
+void deleteList(Node*& head) {
+    while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
 
 
 int main() {
+    Node* head = nullptr;
+    string output;
+    vector<int> values = {1,2,3,4,5,6,7,8,9};
     cout << "TEST 1(correct)" << endl;
-    int n = 5;
-    Node nodes[n];
-    for (int i = 0; i < n; i++) {
-        nodes[i].data = i;
-    }
-    for (int i = 0; i < n -1; i++) {
-        nodes[i].next = &nodes[i+1];
-    }
-    nodes[n-1].next = nullptr;
-    cout << checkList(&nodes[0], nodes, n) << endl;
-    cout << "TEST 2(early null)" << endl;
-    for (int i = 0; i < n; i++) {
-        nodes[i].data = i;
-    }
-    for (int i = 0; i < n -1; i++) {
-        nodes[i].next = &nodes[i+1];
-    }
-    nodes[n/2].next = nullptr;
-    nodes[n-1].next = nullptr;
-    cout << checkList(&nodes[0], nodes, n) << endl;
-    cout << "TEST 3(proskok)" << endl;
-    for (int i = 0; i < n; i++) {
-        nodes[i].data = i;
-    }
-    for (int i = 0; i < n -1; i++) {
-        nodes[i].next = &nodes[i+1];
-    }
-    nodes[0].next = nodes[2].next;
-    nodes[n-1].next = nullptr;
-    cout << checkList(&nodes[0], nodes, n) << endl;
-    cout << "TEST 4(pereskok)" << endl;
-    for (int i = 0; i < n; i++) {
-        nodes[i].data = i;
-    }
-    for (int i = 0; i < n -1; i++) {
-        nodes[i].next = &nodes[i+1];
-    }
-    nodes[1].next = nodes[0].next;
-    nodes[n-1].next = nullptr;
-    cout << checkList(&nodes[0], nodes, n) << endl;
-    return 0;
+    head = constructList(values);
+    output = checkList(head, values.size());
+    cout << output << endl;
+    deleteList(head);
+    cout << "TEST 2(proskok)" << endl;
+    head = constructList(values);
+    Node* temp = head->next;
+    Node* err = temp;
+    temp = temp->next->next;
+    err->next = temp;
+    output = checkList(head, values.size());
+    cout << output << endl;
+    deleteList(head);
+    cout << "TEST 3(pereskok)" << endl;
+    head = constructList(values);
+    temp = head->next;
+    err = temp;
+    temp = temp->next;
+    Node* correct = temp->next;
+    temp->next = err;
+    output = checkList(head, values.size());
+    cout << output << endl;
+    temp->next = correct;
+    deleteList(head);
 }
